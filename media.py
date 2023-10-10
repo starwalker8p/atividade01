68e4298d3e72ed3826c5655e789e82e9413e2087
starwalker8p <starwalker8p@gmail.com>
2023-10-10 16:45:34 -0300

    adicionei o input

--- media.py
+++ media.py
@@ -1,3 +1,3 @@
-notas1,notas2,notas3=int(input("nota 1: ")),int(input("nota 2: ")),int(input("nota 3: "))
-media=(notas1+notas2+notas3)/3
+nota1,nota2,nota3=int(input("nota 1: ")),int(input("nota 2: ")),int(input("nota 3: "))
+media=(nota1+nota2+nota3)/3
 print(media)
